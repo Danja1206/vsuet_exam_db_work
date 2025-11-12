0c0407ae016174e3bcee11658f1ffe576b53fafc
Danja1206 <ena-danja1@yandex.ru>
2025-11-12 22:24:28 +0300

    Добавил сервис работы с бизнес-логикой сущностей

---
+++ backend/src/main/kotlin/ru/vsuet/backend/controller/EntityController.kt
@@ -0,0 +1,59 @@
+package ru.vsuet.backend.controller
+
+import org.springframework.web.bind.annotation.*
+import ru.vsuet.backend.model.Order
+import ru.vsuet.backend.model.Product
+import ru.vsuet.backend.model.Seller
+import ru.vsuet.backend.model.dto.*
+import ru.vsuet.backend.service.EntityService
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = ["*"])
+class EntityController(
+    private val entityService: EntityService
+) {
+    @GetMapping("/sellers")
+    fun getSellers(): List<SellerDto> = entityService.getAllSellers()
+
+    @PostMapping("/sellers")
+    fun createSeller(@RequestBody seller: SellerCreateDto): SellerDto = entityService.createSeller(seller)
+
+    @PutMapping("/sellers/{id}")
+    fun updateSeller(@PathVariable id: Long, @RequestBody seller: SellerCreateDto): SellerDto =
+        entityService.updateSeller(id, seller)
+
+    @DeleteMapping("/sellers/{id}")
+    fun deleteSeller(@PathVariable id: Long): Map<String, String> {
+        entityService.deleteSeller(id)
+        return mapOf("message" to "Продавец успешно удален")
+    }
+
+    @GetMapping("/categories")
+    fun getAllCategories(): List<CategoryDto> = entityService.getAllCategories()
+
+    @GetMapping("/products")
+    fun getProducts(): List<ProductDto> = entityService.getAllProducts()
+
+    @PostMapping("/products")
+    fun createProduct(@RequestBody product: ProductCreateDto): ProductDto = entityService.createProduct(product)
+
+    @PutMapping("/products/{id}")
+    fun updateProduct(@PathVariable id: Long, @RequestBody product: Product): Product =
+        entityService.updateProduct(id, product)
+
+    @DeleteMapping("/products/{id}")
+    fun deleteProduct(@PathVariable id: Long): Map<String, String> {
+        entityService.deleteProduct(id)
+        return mapOf("message" to "Товар успешно удален")
+    }
+
+    @GetMapping("/orders")
+    fun getOrders(): List<OrderDto> = entityService.getAllOrders()
+
+    @DeleteMapping("/orders/{id}")
+    fun deleteOrder(@PathVariable id: Long): Map<String, String> {
+        entityService.deleteOrder(id)
+        return mapOf("message" to "Заказ успешно удален")
+    }
+}
